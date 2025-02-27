7fb6fb7e3a79f125df9bac77714b5f330e7ba547
Afee <vimalastephen@MacBookAir.lan>
2025-02-27 13:04:49 -0500

    some basic changes

--- cypress/e2e/home/mainpage.cy.js
+++ cypress/e2e/home/mainpage.cy.js
@@ -1,22 +1,10 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
+
+
 
 describe('example to-do app', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('http://169.61.105.110/medicareAdvantage_sandbox/login')
     cy.get('#mat-input-0').clear('a');
     cy.get('#mat-input-0').type('valli@gmail.com');
@@ -96,9 +84,7 @@ describe('example to-do app', () => {
   // });
 
   
-  /* ==== Test Created with Cypress Studio ==== */
 
-  /* ==== Test Created with Cypress Studio ==== */
   it('new_Test23', function() {
     /* ==== Generated with Cypress Studio ==== */
 
